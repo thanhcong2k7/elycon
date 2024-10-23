b2131a0a358f3d6d0322cb372e6fb9a0fec1c57f
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2024-10-23 13:49:52 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -579,7 +579,7 @@ app.get('/', (req, res) => {
    });
 const port = 3000;
 app.listen(port, () => {
- console.log(`Server is running at http://localhost:${port}`);
+ console.log(`Server is running at port ${port}`);
 });
 
 // The token of your robot to be inserted
